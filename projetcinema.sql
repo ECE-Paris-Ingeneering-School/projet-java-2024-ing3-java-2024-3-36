ebac8d95cc3b1bb6f7ec4f52dcaa9a63e96bd91c
KarimEczema <97537899+KarimEczema@users.noreply.github.com>
2024-03-26 14:59:36 +0100

    ajout bdd

---
+++ projetcinema.sql
@@ -0,0 +1,115 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : mar. 26 mars 2024 à 13:58
+-- Version du serveur : 8.2.0
+-- Version de PHP : 8.2.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `projetcinema`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `billets`
+--
+
+DROP TABLE IF EXISTS `billets`;
+CREATE TABLE IF NOT EXISTS `billets` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `seanceId` int DEFAULT NULL,
+  `clientId` int DEFAULT NULL,
+  `prix` double DEFAULT NULL,
+  `categorie` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `billets`
+--
+
+INSERT INTO `billets` (`id`, `seanceId`, `clientId`, `prix`, `categorie`) VALUES
+(4, 13, 24, 5, 'famille'),
+(5, 3, 4, 10, 'famile'),
+(6, 1, 2, 3, '4');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `clients`
+--
+
+DROP TABLE IF EXISTS `clients`;
+CREATE TABLE IF NOT EXISTS `clients` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `employes`
+--
+
+DROP TABLE IF EXISTS `employes`;
+CREATE TABLE IF NOT EXISTS `employes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) NOT NULL,
+  `position` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `motDePasse` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `employes`
+--
+
+INSERT INTO `employes` (`id`, `nom`, `position`, `email`, `motDePasse`) VALUES
+(2, 'ezaaze', 'aaaaaaaaa', 'aaaa', 'aaaaaa');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `films`
+--
+
+DROP TABLE IF EXISTS `films`;
+CREATE TABLE IF NOT EXISTS `films` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `titre` varchar(255) NOT NULL,
+  `genre` varchar(100) DEFAULT NULL,
+  `duree` int DEFAULT NULL,
+  `description` text,
+  `realisateur` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `films`
+--
+
+INSERT INTO `films` (`id`, `titre`, `genre`, `duree`, `description`, `realisateur`) VALUES
+(2, 'Le Seigneur des Anneaux', 'Fantaisie', 178, 'Un voyage inattendu.', 'Peter Jackson'),
+(3, 'aaaa', 'aa', 230, 'aaaaaa', 'eeeee');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
